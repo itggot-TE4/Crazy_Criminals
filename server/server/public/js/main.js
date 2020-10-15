d29bbeb548839f4e2fe1a1dc52d2ec027c24a29b
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2020-10-15 12:21:26 +0200

    unused function lint

--- server/server/public/js/main.js
+++ server/server/public/js/main.js
@@ -1,43 +1,41 @@
-async function search(){
-    let search = document.getElementById('searchBar').value;
-    console.log(search);
-    // let result = await getAPI(search);
-    let result = await fetch(`https://api.github.com/users/${search}/repos`);
-    // const text = await (handleData(result.json()));
-    const text = await (result.json());
-    console.log(text)
-    handleData(text);
+async function search () { // eslint-disable-line no-unused-vars
+  const search = document.getElementById('searchBar').value
+  console.log(search)
+  // let result = await getAPI(search);
+  const result = await fetch(`https://api.github.com/users/${search}/repos`)
+  // const text = await (handleData(result.json()));
+  const text = await (result.json())
+  console.log(text)
+  handleData(text)
 }
 
-function handleData(repositories){
-    for(let repo of repositories) {
-        console.log(repo);
-        createCard(repo);
-    }
+function handleData (repositories) {
+  for (const repo of repositories) {
+    console.log(repo)
+    createCard(repo)
+  }
 }
 
-function createCard(repo){
+function createCard (repo) {
+  const parent = document.querySelector('#card')
+  const card = parent.content.cloneNode(true)
 
-    let parent = document.querySelector('#card');
-    let card = parent.content.cloneNode(true);
+  const APIname = card.querySelector('p.name')
+  APIname.innerHTML = repo.name
 
-    let APIname = card.querySelector('p.name');
-    APIname.innerHTML = repo.name;
+  const forks = card.querySelector('p.counter')
+  forks.innerHTML = repo.forks
 
-    let forks = card.querySelector('p.counter');
-    forks.innerHTML = repo.forks;
+  console.log(card)
 
-    console.log(card);
-
-    document.querySelector('.cardBox').appendChild(card)
-
+  document.querySelector('.cardBox').appendChild(card)
 }
 
-function generateTemp(){
-    let parent = document.querySelector('#card');
-    let card = parent.content.cloneNode(true);
-    console.log(card);
-    document.querySelector(".cardBox").appendChild(card);
+function generateTemp () { // eslint-disable-line no-unused-vars
+  const parent = document.querySelector('#card')
+  const card = parent.content.cloneNode(true)
+  console.log(card)
+  document.querySelector('.cardBox').appendChild(card)
 }
 
 function sum (a, b) { return a + b }

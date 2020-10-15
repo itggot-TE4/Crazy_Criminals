22988ab5359f62d8b9913a50d23d6bec48152ab4
te4-alexander-kjellberg <alexander.kjellberg@elev.ga.ntig.se>
2020-10-15 14:43:47 +0200

    finished the thing wiuth the yeah

--- server/server/public/js/main.js
+++ server/server/public/js/main.js
@@ -1,11 +1,7 @@
-async function getForks () { // eslint-disable-line no-unused-vars
-  const user = 'itggot'
-  const reponame = 'apl-preparation'
+async function getForks (user, reponame) { // eslint-disable-line no-unused-vars
   const result = await fetch(`https://api.github.com/repos/${user}/${reponame}/forks`)
   const hej = await result.json()
-  console.log(hej)
   handleForkData(hej)
-  // handleRepoData(hej);
 }
 
 function handleForkData (forkData) {
@@ -52,7 +48,11 @@ function createCard (repo) {
   const forks = card.querySelector('p.counter')
   forks.innerHTML = repo.forks
 
-  console.log(card)
+  const gh = card.querySelector('#ghlink')
+  gh.href = repo.html_url
+
+  const forklink = card.querySelector('.forkLink')
+  forklink.href = `/user/${repo.full_name}`
 
   document.querySelector('.cardBox').appendChild(card)
 }
@@ -64,5 +64,8 @@ function generateTemp () { // eslint-disable-line no-unused-vars
   document.querySelector('.cardBox').appendChild(card)
 }
 
-function sum (a, b) { return a + b }
-module.exports = sum
+const url = window.location.pathname.split('/')
+console.log(url)
+if (url[1] === 'user') {
+  getForks(url[2], url[3])
+}

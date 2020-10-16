3f73d7bfd9b83bc344cbe2ec33458e7f86eccfab
te4-alexander-kjellberg <alexander.kjellberg@elev.ga.ntig.se>
2020-10-16 12:22:38 +0200

    fixed tests and stuff?

--- server/server/public/js/main.js
+++ server/server/public/js/main.js
@@ -7,11 +7,11 @@ function run () {
   }
 }
 
- async function getForks (user, reponame) { // eslint-disable-line no-unused-vars
-  const result = await fetch(`/forks/${user}/${reponame}`, {method: 'POST'})
+async function getForks (user, reponame) { // eslint-disable-line no-unused-vars
+  const result = await fetch(`/forks/${user}/${reponame}`, { method: 'POST' })
   const hej = await result.json()
-//   console.log(JSON.p;
-  handleForkData(hej);
+  //   console.log(JSON.p;
+  handleForkData(hej)
 }
 
 function handleForkData (forkData) {
@@ -32,10 +32,10 @@ function createForkCard (fork) {
 
 async function search () { // eslint-disable-line no-unused-vars
   const search = document.getElementById('searchBar').value
-//   console.log(search)
+  //   console.log(search)
   // let result = await getAPI(search);
-//   const result = await fetch(`https://api.github.com/users/${search}/repos`)
-  const result = await fetch(`/search/${search}`, {method: 'GET'})
+  //   const result = await fetch(`https://api.github.com/users/${search}/repos`)
+  const result = await fetch(`/search/${search}`, { method: 'GET' })
   // const text = await (handleData(result.json()));
   const text = await result.json()
   handleData(text)
@@ -73,6 +73,5 @@ function generateTemp () { // eslint-disable-line no-unused-vars
   console.log(card)
   document.querySelector('.cardBox').appendChild(card)
 }
-
 
 run()
